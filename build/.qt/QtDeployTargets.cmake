a8a777feb0ea1b2694ccfa23fbd0ad86b912875e
Claudiu Acosti <74248681+Claudiu203@users.noreply.github.com>
2025-01-17 14:28:35 +0200

    Delete build directory

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_HTTP-Proxy_FILE /home/claudiu/Documents/demo/build/HTTP-Proxy)
-set(__QT_DEPLOY_TARGET_HTTP-Proxy_TYPE EXECUTABLE)
--- build/CMakeFiles/HTTP-Proxy_autogen.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for HTTP-Proxy_autogen.
-# This may be replaced when dependencies are built.
